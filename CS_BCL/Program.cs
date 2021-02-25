2b448cc54c164bdc96091383a2e19d072067742a
seungCheolCha <cscculture95@gmail.com>
2021-02-25 11:24:45 +0900

    초기 커밋.

---
+++ CS_BCL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS_BCL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DateTime_01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DateTime_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(dt);
+
+            DateTime dayForChildren = new DateTime(dt.Year, 5, 5);
+            Console.WriteLine(dayForChildren);
+
+            DateTime start = DateTime.Now;
+            Sum();
+            DateTime end = DateTime.Now;
+
+            long gap = end.Ticks - start.Ticks;
+            Console.WriteLine("Total Ticks : {0}", gap);
+            Console.WriteLine("Millisecond : {0}", (gap / 10000));
+            Console.WriteLine("Second : {0}", ((gap / 10000) / 1000));
+        }
+
+        private static long Sum()
+        {
+            long sum = 0;
+            for (int i = 0; i < 1000000; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
